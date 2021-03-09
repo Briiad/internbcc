33ff364e84811c0a4226759b4a44926d4083709e
Brian Aditya <brianadit33@gmail.com>
2021-03-09 22:45:49 +0800

    carikerja 50%

--- src/pages/Caripekerjaan.js
+++ src/pages/Caripekerjaan.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import '../css/carikerja.css';
 
 function Caripekerjaan() {
     return (
         <div>
-            <h1>Jey</h1>
+            <div className="carikerja-landing">
+                <h1>Cari Pekerjaan apa hari ini?</h1>
+                <div className="search-box">
+                    <form>
+
+                        <div className="forms">
+                            <p> Cari Pekerjaan </p>
+                            <input type="text" name="caripekerjaan" />
+                        </div>
+
+                        <div className="forms">
+                            <p>Lokasi Pekerjaan</p>
+                            <input type="text" name="lokasipekerjaan" />
+                        </div>
+
+                        <div className="range">
+                            <p>Range Gaji</p>
+                            <input type="text"/>
+                            <button>Cari Kerja</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
